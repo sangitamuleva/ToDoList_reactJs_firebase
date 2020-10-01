de915980c77ee6b20517fbfdeda3a3b1b58e0f00
sangita muleva <mulevasangita@gmail.com>
2020-10-01 16:26:04 +0530

    changes firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@ import firebase from "firebase"
 
 
   const firebaseApp=firebase.initializeApp({
-    apiKey: "AIzaSyCyczfhoWkyXofeb-VTGJ2JRzSerOnfdMo",
-    authDomain: "to-do-list-af659.firebaseapp.com",
-    databaseURL: "https://to-do-list-af659.firebaseio.com",
-    projectId: "to-do-list-af659",
-    storageBucket: "to-do-list-af659.appspot.com",
-    messagingSenderId: "101750354108",
-    appId: "1:101750354108:web:99c6e5bc896f19dcec20b9",
-    measurementId: "G-F5WCJ1DLZW"
+   
   });
 
   const db= firebase.firestore();
